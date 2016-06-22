a3056416b0b812879515de0e1bdd4b8c5ba88762
Berlin Sky <pedroyoung8989@gmail.com>
2016-06-22 10:55:11 -0700

    Initial Project

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,13 +1,51 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { ShoppingListComponent } from './shopping-list.component';
 
 @Component({
-    selector: 'my-app',
+    selector: 'app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!!</p>
-       
-    `,
+			<ul>
+				<li 
+					*ngFor="#shoppingListItem of shoppingListItems"
+					(click)="onItemClicked(shoppingListItem)"
+					>
+					{{ shoppingListItem.name }}
+				</li>
+			</ul>
+			<input type="text" 
+				[(ngModel)]="selectedItem.name"
+			/>
+			<input type="text" 
+				#shoppingListItem
+			/>
+			<button 
+				(click)="onAddItem(shoppingListItem)"
+			>Add Item</button>
+			<button 
+				(click)="onDeleteItem()"
+			>Delete Item</button>
+	`,
+	// directives: [ShoppingListComponent]
 })
 export class AppComponent {
+	public shoppingListItems = [
+		{ name: "Apple" }, 
+		{ name: "Orange" },
+		{ name: "Cheese" }
+	];
+
+	public selectedItem = { name: "" };
+
+	onItemClicked( shoppingListItem ) {
+		this.selectedItem = shoppingListItem;
+	}
+
+	onAddItem(shoppingListItem) {
+		this.shoppingListItems.push({ name: shoppingListItem.value });
+	}	
+
+	onDeleteItem() {
+		this.shoppingListItems.splice(this.shoppingListItems.indexOf(this.selectedItem), 1);
+	}
 
 }
